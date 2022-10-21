ba3aa8cdce5590db5fa902bd4f66e168577eb846
JH-Kim <103908144+LannMalaia@users.noreply.github.com>
2022-10-21 14:57:33 +0900

    Removed Secret Key

--- myWebDjangoSite_1/myWebDjangoSite_1/settings.py
+++ myWebDjangoSite_1/myWebDjangoSite_1/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qx^mk$rb*5p7%wge-diugfms)9dn-lujbh5)td_p5z&^*bq4%#'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
