df2bd7b5dbba304b62ccc9e5d5697f3897d62d20
JH-Kim <103908144+LannMalaia@users.noreply.github.com>
2022-10-21 14:56:37 +0900

    Removed Secret Key

--- DjangoWebProject1/DjangoWebProject1/DjangoWebProject1/settings.py
+++ DjangoWebProject1/DjangoWebProject1/DjangoWebProject1/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '54f16f09-5c41-4f1f-a376-1adcee5dc9fc'
+SECRET_KEY = 'put your secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
